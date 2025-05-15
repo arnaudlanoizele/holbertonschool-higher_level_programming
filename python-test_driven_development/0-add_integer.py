c4e10d8a9196e7d920cc2b9e3731bc5937130e38
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-15 17:53:34 +0200

    docstring module 5/ docstring 3

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-""" Module to add two integers."""
+"""
+This module provides a function to add two integers.
+The function 'add_integer' takes two parameters
+and returns their sum.
+"""
 def add_integer(a, b=98):
-    """Return the sum of two integers."""
+    """
+    Return the sum of two integers.
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
