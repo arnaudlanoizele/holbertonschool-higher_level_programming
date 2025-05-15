44cf90ffad4bb6c673d4412c70f3923b6f0e2630
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-15 16:06:06 +0200

    lowercase wrong type

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             result.append(0)
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
             result.append(0)
         except IndexError:
             print("out of range")
