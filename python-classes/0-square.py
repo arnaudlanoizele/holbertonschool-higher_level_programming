33d913cb166cd1caf27fde3b6c8ba63241e31e9e
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-19 11:20:52 +0200

    test3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,17 @@
-#!/ust/bin/python3
-"""
+#!/usr/bin/python3
+'''
 This module provides a single class defining a square.
-"""
+'''
+
 
 class Square:
-    """
+    '''
     This is a blueprint of a class that defines a square.
 
     Args:
         None
-    
+
     Attributes:
                 None
-    """
+    '''
     pass
