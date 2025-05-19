bcf4b599114bb861d651a316a378e1d6b9ed6d78
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-19 18:35:25 +0200

    3. Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,15 +6,14 @@ This module provides a single class defining a square
 
 class Square:
     '''
-    This class defines a square based on an attribute named size.
-    Size must be an int and positive.
-    Size is private to allow more control of square.
+    This class defines a square based on its size.
+    Size must be an integer and positive.
 
     Args:
         size (int): The size of the square, must be positive
 
-    Attributes:
-                size (int): The size of the square, must be positive
+    Attribute:
+                size (int): The rize of the square, must be positive
     '''
     def __init__(self, size=0):
         if not isinstance(size, int):
@@ -22,3 +21,11 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        '''
+        This method will calculate the area of the square
+        based on the formula : area = size * size
+        '''
+        area = self.__size**2
+        return area
