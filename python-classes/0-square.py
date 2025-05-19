65f72d6394478c0db608d7e0536d5913b533de42
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-19 11:15:22 +0200

    0. My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/ust/bin/python3
+"""
+Define a class Square with no attributes or methods.
+"""
+
+class Square:
+    """
+    An empty class that defines a square.
+
+    Args:
+        None
+    
+    Attributes:
+        None
+    """
+    pass
