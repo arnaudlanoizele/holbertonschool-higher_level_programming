fb41dd3ea993933fbbdb4273686675b72bc1fa3b
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-20 16:47:43 +0200

    0. Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+This module provides a simple class defining a rectangle
+'''
+
+
+class Rectangle:
+    '''
+    This a blueprint of a class that define a rectangle
+    
+    Args:
+        none
+
+    Attributes:
+                none
+    '''
+    pass
