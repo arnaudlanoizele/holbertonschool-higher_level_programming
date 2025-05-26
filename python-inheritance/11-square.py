c64f3185f52a63af1f3d068d585cdd40110cf141
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-26 19:28:19 +0200

    double underscore linea 17 and linea 18

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,8 +14,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().init__(size, size)
-        self.size = size
+        super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         return super().__str__().replace("Rectangle", "Square")
