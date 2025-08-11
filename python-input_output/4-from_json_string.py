9537c2681f16f955f457f48c8f909f9affbe5188
arnaudlanoizele <10476@holbertonstudents.com>
2025-08-11 11:44:19 +0200

    4. From JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pytihn3
+"""Module for JSON string to object"""
+
+
+def from_json_string(my_str):
+    """Convert a JSON string to a Python object"""
+    import json
+    return json.loads(my_str)
