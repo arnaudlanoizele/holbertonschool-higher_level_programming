30960f9c735bb81ce95b3044b7d67947492e2c0c
arnaudlanoizele <10476@holbertonstudents.com>
2025-08-11 11:12:35 +0200

    1. Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for writing to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a UTF-8 encoded text to a file and returns the number of characters written
+
+    Args:
+        filename (str): The name of the file to write to. Defaults to empty string
+        """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
