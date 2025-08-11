4d685445d4911c314884d988c3906bbc288f8843
arnaudlanoizele <10476@holbertonstudents.com>
2025-08-11 11:14:30 +0200

    line too long

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,10 +3,12 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a UTF-8 encoded text to a file and returns the number of characters written
+    """Writes a UTF-8 encoded text to a file and returns 
+    the number of characters written
 
     Args:
-        filename (str): The name of the file to write to. Defaults to empty string
+        filename (str): The name of the file to write to.
+        Defaults to empty string
         """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
