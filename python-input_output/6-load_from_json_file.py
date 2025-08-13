8c71f19df79ed59123037a4605eba8019018988a
arnaudlanoizele <10476@holbertonstudents.com>
2025-08-13 20:31:16 +0200

    test

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,7 +2,8 @@
 """Module for loading from JSON file"""
 import json
 
-def load_from_json(filename):
-    """Function that creates an object from a JSON file"""
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a JSON file"""
     with open(filename) as f:
         return json.load(f)
