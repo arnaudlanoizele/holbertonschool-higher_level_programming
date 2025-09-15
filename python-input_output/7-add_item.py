fdd14de262f2d6f84c37ba933a8a8e5d847eed07
arnaudlanoizele <10476@holbertonstudents.com>
2025-09-15 09:55:22 +0200

    else

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -15,6 +15,7 @@ my_list = []
 
 if os.path.exists(filename):
     my_list = load_from_json_file(filename)
-
+else:
+    my_list = []
 my_list.extend(sys.argv[1])
 save_to_json_file(my_list, filename)
