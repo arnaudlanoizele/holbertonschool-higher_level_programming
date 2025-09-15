ec55906ed1f98b48c3c8ca049a1d3aa5821e8ae0
arnaudlanoizele <10476@holbertonstudents.com>
2025-09-15 10:04:16 +0200

    8. Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+"""
+Description : This module contain a script that returns the dictionnary
+description with simple data structure (List, dictionary, string, integer)
+"""
+
+
+def class_to_json(obj):
+    """
+    A funtion that returns the dictionnary description with simple data
+    structure (list, dictionary, string, integer)
+    """
+    return obj.__dict__
